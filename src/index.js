e51254c4036725ade6e5e9fde0798a54026e4c61
trondeh80 <trond.erling@gmail.com>
2017-06-05 20:32:56 +0200

    Fixed movement. Added some css and score.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 class Game {
 
     constructor() {
+
         this.setupCanvas();
+
+        this.addKeyListener();
+
+        this.addMouseListener();
+
+        this.addStartPosition();
+
+        // Start render loop
+        this.loop(0);
     }
 
     setupCanvas() {
-
-        this.density = 10;
+        this.gameover = false;
+        this.highscores = [];
+        this.density = 20;
         this.speed = 9; // Difficulty
-        this.gridsize = [1080, 800];
+        this.gridsize = [1080, 600];
         this.drawable = {};
+        this.score = 0;
         this.lastFrameMs = 0;
         this.direction = 1; // 0 = up, 1 = right, 2 = down, 3 = left.
 
@@ -19,25 +31,29 @@ class Game {
         this.canvas.width = this.gridsize[0];
         this.canvas.height = this.gridsize[1];
         this.dom.appendChild(this.canvas);
-
-        this.addKeyListener();
-
-        this.addStartPosition();
-
-        // Start loop
-        this.loop(0);
     }
 
     addKeyListener() {
         window.addEventListener('keydown', event => {
-            if (event.key === 'ArrowUp') {
+            if (event.key === 'ArrowUp' && this.direction !== 2) {
                 this.direction = 0;
-            } else if (event.key === 'ArrowRight') {
+            } else if (event.key === 'ArrowRight' && this.direction !== 3) {
                 this.direction = 1;
-            } else if (event.key === 'ArrowDown') {
+            } else if (event.key === 'ArrowDown' && this.direction !== 0) {
                 this.direction = 2;
-            } else if (event.key === 'ArrowLeft') {
+            } else if (event.key === 'ArrowLeft' && this.direction !== 1) {
                 this.direction = 3;
+            }
+        });
+    }
+
+    addMouseListener() {
+        window.addEventListener('mouseup', event => {
+            if (this.gameover) {
+                this.addStartPosition();
+                this.gameover = false;
+                this.speed = 9; // Difficulty
+                this.score = 0;
             }
         });
     }
@@ -59,13 +75,9 @@ class Game {
 
     getFood() {
         if (!this.drawable.food) {
-            const foodPos = {
+            this.drawable.food = {
                 x: Math.floor(Math.random() * (this.gridsize[0] / this.density)),
                 y: Math.floor(Math.random() * (this.gridsize[1] / this.density))
-            };
-            this.drawable.food = {
-                x: foodPos.x,
-                y: foodPos.y
             }
         }
         return this.drawable.food;
@@ -74,8 +86,9 @@ class Game {
     drawFood() {
         const food = this.getFood();
         this.getContext().fillStyle = 'red';
-        this.getContext().fillRect(food.x * this.density, food.y * this.density, 10, 10);
+        this.getContext().fillRect(food.x * this.density, food.y * this.density, this.density, this.density);
     }
+
 
     intersects() {
         // Intersects food:
@@ -83,29 +96,47 @@ class Game {
             this.drawable.food.y === this.drawable.player.position.y) {
             this.drawable.player.eaten++;
             this.drawable.food = null;
+
+            this.score = Math.floor((this.drawable.player.eaten + 1) * (this.drawable.player.eaten * (this.speed)));
+
             if (this.drawable.player.eaten % 5 === 0) {
                 this.speed = this.speed * 0.8; // Increase speed by 20%;
+            }
+        }
+
+        for (let z = this.drawable.player.eaten; z > 0; z--) {
+            let move = this.drawable.player.moves[z];
+            if (z > 3 && this.drawable.player.position.x === move.x && this.drawable.player.position.y === move.y) {
+                this.gameover = true;
             }
         }
     }
 
     loop(timestamp) {
 
-        // console.log('timestamp: ', timestamp);
-
         // Reset board:
         this.getContext().fillStyle = 'white';
         this.getContext().fillRect(0, 0, this.gridsize[0], this.gridsize[1]);
 
-        this.drawFood(); // draw new og existing food item
+        if (this.gameover) {
 
-        this.movePlayer(); // updates player position.
+            this.drawGameOver();
 
-        this.intersects(); // handles intersection. (extends snake, or player looses)
+        } else {
 
-        this.drawPlayer();
+            this.drawFood(); // draw new og existing food item
 
-        // Wait to simulate correct timing.
+            this.movePlayer(); // updates player position.
+
+            this.intersects(); // handles intersection. (extends snake, or player looses)
+
+            this.drawPlayer();
+
+            this.setScore(this.score);
+
+        }
+
+        // Wait to simulate correct timing. TODO: Fix this...
         let waiting = true;
         let ts = Date.now();
         while (waiting) {
@@ -118,6 +149,71 @@ class Game {
         window.requestAnimationFrame(this.loop.bind(this));
     }
 
+    movePlayer() {
+
+        for (let z = this.drawable.player.eaten + 1; z > 0; z--) {
+            this.drawable.player.moves[z] = {
+                x: this.drawable.player.moves[(z - 1)].x,
+                y: this.drawable.player.moves[(z - 1)].y
+            };
+        }
+
+        const {x, y} = this.drawable.player.position;
+
+        if (this.direction === 1) { // right
+            this.drawable.player.position.x = (x + 1 === this.gridsize[0] / this.density) ? 0 : x + 1;
+        } else if (this.direction === 3) { // left
+            this.drawable.player.position.x = (x - 1 < 0) ? this.gridsize[0] / this.density : x - 1;
+        } else if (this.direction === 0) {
+            this.drawable.player.position.y = (y - 1 < 0) ? this.gridsize[1] / this.density : y - 1;
+        } else if (this.direction === 2) {
+            this.drawable.player.position.y = (y + 1 === this.gridsize[1] / this.density) ? 0 : y + 1;
+        }
+
+        this.drawable.player.moves[0] = {
+            direction: this.direction,
+            x: this.drawable.player.position.x,
+            y: this.drawable.player.position.y
+        };
+    }
+
+
+    setScore(score) {
+        document.querySelector('.score-points').innerHTML = score;
+    }
+
+    setHighscore() {
+        let highest = this.highscores.reduce((memo, item) => {
+            return item.score > memo ? item.score : memo;
+        }, 0);
+        if (this.score > highest) {
+            document.querySelector('.high-score-points').innerHTML = this.score;
+            this.highscores.push({ score: this.score });
+        }
+    }
+
+    drawGameOver() {
+
+        this.setHighscore();
+
+        const ctx = this.getContext();
+        ctx.font = '48px serif';
+        ctx.fillStyle = '#000';
+        ctx.drawStyle = '#000';
+        ctx.fillText('GAME OVER', this.gridsize[0] / 2 - 150, this.gridsize[1] / 2, this.gridsize[1] / 2);
+    }
+
+    drawPlayer() {
+        const ctx = this.getContext();
+        ctx.fillStyle = '#000000';
+        const playerBits = this.drawable.player.eaten + 1;
+        for (let v = 0; v < playerBits; v++) {
+            let move = this.drawable.player.moves[v];
+            ctx.fillRect(move.x * this.density, move.y * this.density, this.density, this.density); // density = 10 ie.
+        }
+    }
+
+
     getContext() {
         if (!this.ctx) {
             this.ctx = this.canvas.getContext('2d');
@@ -125,65 +221,6 @@ class Game {
         return this.ctx;
     }
 
-    movePlayer() {
-
-        const {x, y} = this.drawable.player.position;
-
-        if (this.direction === 1) { // right
-            this.drawable.player.position.x = (x + 1 >= this.gridsize[0] / this.density) ? 0 : x + 1;
-        } else if (this.direction === 3) { // left
-            this.drawable.player.position.x = (x - 1 <= 0) ? this.gridsize[0] / this.density : x - 1;
-        } else if (this.direction === 0) {
-            this.drawable.player.position.y = (y - 1 <= 0) ? this.gridsize[1] / this.density : y - 1;
-        } else if (this.direction === 2) {
-            this.drawable.player.position.y = (y + 1 >= this.gridsize[1] / this.density) ? 0 : y + 1;
-        }
-
-        if (this.drawable.player.moves[0].direction !== this.direction) {
-            this.drawable.player.moves.splice(0, 0, {
-                direction: this.direction,
-                x: this.drawable.player.position.x,
-                y: this.drawable.player.position.y
-            });
-        }
-    }
-
-    drawPlayer() {
-        let {x, y} = this.drawable.player.position;
-        const ctx = this.getContext();
-        ctx.fillStyle = '#000000';
-
-        // draw current position!
-        ctx.fillRect(x * this.density, y * this.density, this.density, this.density); // density = 10 ie.
-
-        // draw tail TODO: fix this using vector!
-        let drawn = this.drawable.player.eaten;
-        let moves = 0;
-        while (drawn > 0) {
-            let move = this.drawable.player.moves[moves];
-            let dir = move.direction;
-
-            if (dir === 1) { // right
-                x = x - 1;
-            } else if (dir === 3) {
-                x = x + 1;
-            } else if (dir === 0) {
-                y = y + 1;
-            } else if (dir === 2) {
-                y = y - 1;
-            }
-
-            ctx.fillRect(x * this.density, y * this.density, this.density, this.density); // density = 10 ie.
-
-            if (x === move.x && y === move.y) {
-                moves++;
-            }
-
-            drawn--;
-        }
-
-
-    }
 }
 
 const game = new Game();
